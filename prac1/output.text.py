932d07a24e62dcda82f4838e7602b5a23cdbaa2b
aidaa2007 <aidaakrmbb@outlook.com>
2026-02-06 04:47:32 +0600

    .

---
+++ prac1/output.text.py
@@ -0,0 +1,12 @@
+#print text
+print("Hello World!")
+#printing many times
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+#using single or double quotes
+print("This will work!")
+print('This will also work!')
+#printing without newline
+print("Hello World!", end=" ")
+print("I will print on the same line.")
