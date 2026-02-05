b219dbd8f17a9a22385e80c00645c3a775256511
aidaa2007 <aidaakrmbb@outlook.com>
2026-02-06 04:05:43 +0600

    python

--- prac1/py-basics.py
+++ prac1/py-basics.py
@@ -1,2 +1,21 @@
-name = input()
-print(f"Hello {name}")
+print("Hello, World!")
+
+if 5 > 2:
+  print("Five is greater than two!")
+  
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
